c935f27d52e837aa38ec61d569bea367ea3533e2
fregie8 <50201096+fregie8@users.noreply.github.com>
2024-08-21 23:36:45 +0700

    Update app.go

--- app.go
+++ app.go
@@ -3,10 +3,24 @@ package main
 import (
 	"html/template"
 	"log"
+	"net/http"
+
+	"github.com/julienschmidt/httprouter"
 )
 
 func main() {
 	log.Println("======= Start App =========")
-	var template *template.Template
-	template.Lookup("test")
+	router := httprouter.New()
+	router.GET("/", callHTML)
+}
+
+func callHTML(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
+	var templateObject *template.Template
+	testTemplate := templateObject.Lookup("test")
+	if testTemplate != nil {
+		if err := testTemplate.Execute(w, nil); err != nil {
+			log.Println(err)
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+		}
+	}
 }
