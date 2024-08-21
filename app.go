38b5bc174c90eefbec3ef3ded7afee357f875cde
fregie8 <50201096+fregie8@users.noreply.github.com>
2024-08-21 23:40:06 +0700

    Update app.go

--- app.go
+++ app.go
@@ -11,10 +11,11 @@ import (
 func main() {
 	log.Println("======= Start App =========")
 	router := httprouter.New()
-	router.GET("/", callHTML)
+	router.GET("/", CallHTML)
+	router.GET("/ping", HandlePing)
 }
 
-func callHTML(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
+func CallHTML(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
 	var templateObject *template.Template
 	testTemplate := templateObject.Lookup("test")
 	if testTemplate != nil {
@@ -23,4 +24,13 @@ func callHTML(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
 			http.Error(w, err.Error(), http.StatusInternalServerError)
 		}
 	}
+}
+
+func HandlePing(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
+	pingStr := []byte(`{"ping":"pong"}`)
+	common.NewAjax(w, r, []byte(pingStr), http.StatusOK)
+	w.Header().Set("Access-Control-Allow-Origin", "*")
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	w.Write(pingStr)
 }
